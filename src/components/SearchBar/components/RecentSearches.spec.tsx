2fb0043b8afcaeb6b8d463f6e2a92c433fee9107
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-01-07 18:47:01 -0300

    test: adiciona testes para o componente 'Recent Searches'

---
+++ src/components/SearchBar/components/RecentSearches.spec.tsx
@@ -0,0 +1,37 @@
+import { fireEvent, render, screen } from "@testing-library/react-native";
+import { RecentSearches } from "./RecentSearches";
+
+const FIRST_TEXT_MOCK = "Hello";
+const SECOND_TEXT_MOCK = "World";
+
+describe("Recent Searches Component", () => {
+	const removeFromRecentSearchesMock = jest.fn();
+
+	beforeAll(() =>
+		render(
+			<RecentSearches
+				recentSearches={[FIRST_TEXT_MOCK, SECOND_TEXT_MOCK]}
+				removeFromRecentSearches={removeFromRecentSearchesMock}
+			/>
+		)
+	);
+
+	describe("Render", () => {
+		it("should render correctly", () => {
+			expect(screen.getByText("Latest searches")).toBeTruthy();
+			expect(screen.getByText(FIRST_TEXT_MOCK)).toBeTruthy();
+			expect(screen.getByText(SECOND_TEXT_MOCK)).toBeTruthy();
+		});
+	});
+
+	describe("Interactions", () => {
+		describe("Press", () => {
+			it("should call the function passed by props to the Word Already Searches Component", () => {
+				const removeButton = screen.getAllByLabelText("Remove");
+				fireEvent.press(removeButton[0]);
+
+				expect(removeFromRecentSearchesMock).toHaveBeenCalled();
+			});
+		});
+	});
+});
